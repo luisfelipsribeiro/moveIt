c257a536d9df94e74a3fab377ef61a9ca81ff6a6
Luis Felipe <luis.moreira@dasa.com.br>
2021-10-18 11:09:15 -0300

    creation on move it app for web version

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,60 @@
+import { createContext, useState, ReactNode } from 'react' 
+
+import challenges from '../../challenges.json'
+
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
+
+interface ChallengeContextData {
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
+    activeChallenge: Challenge;
+    levelUp: () => void;
+    startNewChallenge: () => void;
+}
+
+interface ChallengesProviderProps {
+    children: ReactNode;
+}
+
+export const ChallengesContext = createContext({} as ChallengeContextData);
+
+export function ChallengesProvider ({ children}: ChallengesProviderProps) {
+
+    const [level, setLevel] = useState(1)
+    const [currentExperience, setCurrentExperience] = useState(0);
+    const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+    const [activeChallenge, setActiveChallenge] = useState(null)
+
+    function levelUp () {
+        setLevel(level + 1);
+    }
+
+    function startNewChallenge () {
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengeIndex];
+
+        setActiveChallenge(challenge);
+    }
+
+    return( 
+
+        <ChallengesContext.Provider
+            value={{
+                level, 
+                currentExperience, 
+                challengesCompleted,
+                activeChallenge, 
+                levelUp,
+                startNewChallenge,
+            }}
+        >
+            { children}
+        </ChallengesContext.Provider>
+    );
+}
